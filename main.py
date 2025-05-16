3d9c4402d3163c60d2355d8b1676a35a4a3c50f9
sinoro <signourmelo@gmail.com>
2025-05-16 12:55:58 +0800

    E: now prints a summarized report in the console

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from stats import count_words
+from stats import sort_count_chars
+
+print("============ BOOKBOT ============\n" \
+"Analyzing book found at books/frankenstein.txt...\n"
+"----------- Word Count ----------")
+
 count_words("./books/frankenstein.txt")
 
-from stats import count_chars
-count_chars("./books/frankenstein.txt")
+print("--------- Character Count -------")
+
+sort_count_chars("./books/frankenstein.txt")
+
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -4,25 +4,25 @@ def count_words(path):
         file_contents = f.read()
     # counts the words in a file
     num_words = len(file_contents.split())
-    print(f"{num_words} words found in the document")
+    print(f"Found {num_words} total words")
 
+# def count_chars(path):
 
-# def full_character_count(path):
 #     with open(path) as f:
-#         # reads the file
+#     # reads the file
 #         file_contents = f.read()
-    
-#     full_text = file_contents.lower()
-#     characters_dict = {}
-    
-#     # print(words)
-#     for char in full_text:
-#         # print(len(char))
-#         characters_dict[char] += len(char)
-    
-#     return characters_dict
 
-def count_chars(path):
+#     file_contents= file_contents.lower()
+#     characters_dict = {}
+
+#     for char in file_contents:
+#         if char in characters_dict:
+#             characters_dict[char] += 1
+#         else:
+#             characters_dict[char] = 1
+#     print(characters_dict)
+
+def sort_count_chars(path):
 
     with open(path) as f:
     # reads the file
@@ -31,10 +31,33 @@ def count_chars(path):
     file_contents= file_contents.lower()
     characters_dict = {}
 
+
     for char in file_contents:
         if char in characters_dict:
             characters_dict[char] += 1
         else:
             characters_dict[char] = 1
-    print(characters_dict)
+    
+    dictionary_list = []
+
+    for key,value in characters_dict.items():
+        my_dict = {"char":key, "num":value}
+        dictionary_list.append(my_dict)
+    
+    def sort_on(dict):
+        return dict["num"]
+    
+    dictionary_list.sort(reverse=True, key=sort_on)
+    # print(dictionary_list)
+
+    for dict in dictionary_list:
+        if dict["char"].isalpha() == True:
+            print(f"{dict["char"]}: {dict["num"]}")
+
+    
+
+
+
+
+
         
